ead02628920fb81526162ae7b6063e9955bdf823
Daniel Owen <daniel.owen@advancedspace.com>
2024-07-24 11:20:54 -0600

    Added in kepler orbits for planets, updated propagator

--- razorback/kepler.jl
+++ razorback/kepler.jl
@@ -1,41 +1,161 @@
 # Initial test at basic kepler's problem functions (prop and state <=> element)
 # Author: Daniel Owen
 # Created: May 23, 2024
-# Edited: May 23, 2024
+# Edited: July 23, 2024
 # Version: 0.1
 
+using LinearAlgebra
+
 function state_to_elements(R, V, μ)
-    # calc h
-    # calc v
-    # calc a from energy
-    # e from h equation
-    # look up i, raan, argPeri
+    H = cross(R, V)
+    h = norm(H)
+
+    N = cross([0, 0, 1], H)
+    n = norm(N)
+
+    r = norm(R)
+    v = norm(V)
+
+    ϵ = v^2/2 - μ/r
+    E = ((v^2 - μ/r)*R - dot(R, V)*V)/μ
+    e = norm(E)
+
+    a = -μ/2/ϵ
+
+    i = acos(H[3]/h)
+    
+    Ω = acos(N[1]/n)
+    if N[2] < 0
+        Ω = 2*π - Ω
+    end
+
+    ω = acos(dot(N, E)/n/e)
+    if E[3] < 0
+        ω = 2*π - ω
+    end
+
+    θ = acos(dot(E, R)/e/r)
+    if dot(R, V) < 0
+        θ = 2*π - θ
+    end
+
+    special_case = "none"
+    λ_true = 0
+    u = 0
+    ω_true = 0
+    if e == 0
+        if i == 0
+            special_case = "circ_eq"
+            λ_true = acos(R[1]/r)
+            if R[2] < 0
+                λ_true = 2*π - λ_true
+            end
+        else
+            special_case = "circ_inc"
+            u = acos(dot(N, R)/n/r)
+            if R[3] < 0
+                u = 2*π - u
+            end
+        end
+    else
+        if i == 0
+            special_case = "ellip_eq"
+            ω_true = acos(E[1]/e)
+            if E[2] < 0
+                ω_true = 2*π - ω_true
+            end
+        end
+    end
+
+    OE = Dict("a" => a, 
+              "e" => e, 
+              "i" => i, 
+              "Ω" => Ω, 
+              "ω" => ω, 
+              "θ" => θ,
+              "ω true" => ω_true,
+              "λ true" => λ_true,
+              "u" => u, 
+              "special_case" => special_case)
+    return OE
 end
 
 function elements_to_state(OE, μ)
-    h = sqrt(mu*OE["a"]*(1 - OE["e"]^2))
-    r = h^2/μ/(1 + OE["e"]*cos(OE["θ"]))
+    θ = OE["θ"]
+    Ω = OE["Ω"]
+    ω = OE["ω"]
+
+    special_case = OE["special_case"]
+    if special_case != "none"
+        if special_case == "circ_eq"
+            Ω = 0
+            ω = 0
+            θ = OE["λ true"]
+        elseif special_case == "circ_inc"
+            ω = 0
+            θ = OE["u"]
+        else
+            Ω = 0
+            ω = OE["ω true"]
+        end
+    end
+
+    h = sqrt(μ*OE["a"]*(1 - OE["e"]^2))
+    r = h^2/μ/(1 + OE["e"]*cos(θ))
     
     ϵ = -μ/2/OE["a"]
     v = sqrt(2*(ϵ + μ/r))
 
-    R = [r*cos(OE["θ"]), r*sin(OE["θ"]), 0]
-    V = [v*-sin(OE["θ"]), v*cos(OE["θ"]), 0]
+    # println(ϵ)
+    # println(v)
+
+    R_perifocal = [r*cos(θ), r*sin(θ), 0]
+    V_perifocal = [v*-sin(θ), v*cos(θ), 0]
     
     # rotate w/ matrix
+    R1 = [cos(Ω) sin(Ω) 0; -sin(Ω) cos(Ω) 0; 0 0 1]'
+    R2 = [1 0 0; 0 cos(OE["i"]) sin(OE["i"]); 0 -sin(OE["i"]) cos(OE["i"])]'
+    R3 = [cos(ω) sin(ω) 0; -sin(ω) cos(ω) 0; 0 0 1]'
+
+    R = R1*R2*R3*R_perifocal
+    V = R1*R2*R3*V_perifocal
+
+    return (R, V)
+end
+
+function kep_E(M, e)
+    if M > π || -π < M < 0 
+        E_old = M - e
+    else
+        E_old = M + e
+    end
+
+    for i in 1:100
+        E = E_old + (M - E_old + e*sin(E_old))/(1 - e*cos(E_old))
+        
+        if E - E_old < 1e-9
+            break
+        end
+        
+        E_old = E
+    end
+
+    return E_old
 end
 
 function propogate(R, V, μ, dT)
     OE = state_to_elements(R, V, μ)
     T = 2*π/sqrt(μ)*OE["a"]^(3/2)
 
-    # get Ei
-    Mi = Ei - OE["e"]*sin(Ei)
+    e = OE["e"]
+    Ei = 2*atan(sqrt((1 - e)/(1 + e))*tan(OE["θ"]/2))
+    Mi = Ei - e*sin(Ei)
     Mf = Mi + dT*2*pi/T
+    Mf = mod(Mf, 2*π)
 
-    # get new_theta
+    Ef = kep_E(Mf, e)
 
-    OE["θ"] = new_theta
+    OE["θ"] = 2*atan(sqrt((1 + e)/(1 - e))*tan(Ef/2))
     
     return elements_to_state(OE, μ)
 end
--- razorback/planets.jl
+++ razorback/planets.jl
@@ -4,14 +4,23 @@
 # Edited: July 5, 2024
 # Version: 0.2
 
-function test_planet()
-    R1, V1 = get_state("Earth", 0)
-    R2, V2 = get_state("Earth", 365.25/4*86400)
-    R3, V3 = get_state("Earth", 365.25/2*86400)
+include("kepler.jl")
 
-    println(R1)
-    println(R2)
-    println(R3)
+function test_planet()
+    R1, V1 = get_state_circ("Earth", 0)
+    R2, V2 = get_state_circ("Earth", 365.25/4*86400)
+    R3, V3 = get_state_circ("Earth", 365.25/2*86400)
+
+    R4, V4 = get_state_kep("Earth", 0)
+    R5, V5 = get_state_kep("Earth", 365.25)
+
+    # println(R1)
+    # println(R2)
+    # println(R3)
+    println(R4)
+    println(V4)
+    println(R5)
+    println(V5)
 end
 
 ids = Dict(1 => "Mercury",
@@ -23,19 +32,103 @@ ids = Dict(1 => "Mercury",
            7 => "Uranus",
            8 => "Nepture")
 
-sma = Dict("Mercury" => 0.38709927, 
-           "Venus" => 0.72333566, 
-           "Earth" => 1.00000261, 
-           "Mars" => 1.52371034,
-           "Jupiter" => 5.20288700,
-           "Saturn" => 9.53667594,
-           "Uranus" => 19.18916464,
-           "Nepture" => 30.06992276)
+sma = Dict("Mercury" => 0.387098309, 
+           "Venus" => 0.72332982, 
+           "Earth" => 1.0000010178, 
+           "Mars" => 1.52367934,
+           "Jupiter" => 5.202603191,
+           "Saturn" => 9.554909595,
+           "Uranus" => 19.218446061,
+           "Nepture" => 30.11038687)
+
+ecc = Dict("Mercury" => 0.205631752, 
+           "Venus" => 0.006771882, 
+           "Earth" => 0.016708617, 
+           "Mars" => 0.093400620,
+           "Jupiter" => 0048494851,
+           "Saturn" => 0.055508622,
+           "Uranus" => 0.046295898,
+           "Nepture" => 0.008988095)
+        
+inc = Dict("Mercury" => 7.00798625, 
+           "Venus" => 3.39446619, 
+           "Earth" => 0.000, 
+           "Mars" => 1.84972648,
+           "Jupiter" => 1.30326966,
+           "Saturn" => 2.48887810,
+           "Uranus" => 0.77319617,
+           "Nepture" => 1.76995221)
+
+RAAN = Dict("Mercury" => 48.33089304, 
+            "Venus" => 76.67992019, 
+            "Earth" => 0.000, 
+            "Mars" => 49.55809321,
+            "Jupiter" => 100.46444064,
+            "Saturn" => 113.66552370,
+            "Uranus" => 74.00594723,
+            "Nepture" => 131.78405702)
+
+long_peri = Dict("Mercury" => 77.45611904, 
+                 "Venus" => 131.56370724, 
+                 "Earth" => 102.93734851, 
+                 "Mars" => 336.06023398,
+                 "Jupiter" => 14.33130924,
+                 "Saturn" => 93.05678728,
+                 "Uranus" => 173.00515922,
+                 "Nepture" => 48.12369050)
+
+long_true = Dict("Mercury" => 252.25090551, 
+                 "Venus" => 181.97980084, 
+                 "Earth" => 100.46644851, 
+                 "Mars" => 355.43327463,
+                 "Jupiter" => 34.35148392,
+                 "Saturn" => 50.07747138,
+                 "Uranus" => 314.05500511,
+                 "Nepture" => 304.34866548)
 
 μ_sun = 1.32712440e11;
 AU = 149597870.7
 
-function get_state(planet, time)
+function get_state_kep(planet, time)
+    a = sma[planet]*AU
+    e = ecc[planet]
+    i = deg2rad(inc[planet])
+    Ω = deg2rad(RAAN[planet])
+    ω_true = deg2rad(long_peri[planet])
+    ω = ω_true - Ω
+    λ_true = deg2rad(long_true[planet])
+    θ = λ_true - Ω - ω
+    u = ω + θ
+
+    special_case = "none"
+    if e == 0
+        if i == 0
+            special_case = "circ_eq"
+        else
+            special_case = "circ_inc"
+        end
+    else
+        if i == 0
+            special_case = "ellip_eq"
+        end
+    end
+    
+    coe = Dict("a" => a, 
+               "e" => e, 
+               "i" => i, 
+               "Ω" => Ω, 
+               "ω" => ω, 
+               "θ" => θ,
+               "ω true" => ω_true,
+               "λ true" => λ_true,
+               "u" => u, 
+               "special_case" => special_case)
+
+    R_j2000, V_j2000 = elements_to_state(coe, μ_sun)
+    return propogate(R_j2000, V_j2000, μ_sun, time)
+end
+
+function get_state_circ(planet, time)
     r = sma[planet]*AU
     v = sqrt(μ_sun/r)
 
@@ -48,3 +141,5 @@ function get_state(planet, time)
 
     return (R, V)
 end
+
+test_planet()
